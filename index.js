1821aa47215c518b3b67a83dd9ef950104595a23
lakshman-nanchari <lakshmannanchari96@gmail.com>
2023-11-03 08:35:33 +0000

    export and import first Name of people

---
+++ index.js
@@ -0,0 +1,11 @@
+const peoplesNames = require("../country/state/city/index");
+
+const getFirstNames = require("../utilities/utils/index");
+
+const getPeopleInCity = (peoplesNames) => {
+  return getFirstNames(peoplesNames);
+};
+
+module.exports = getPeopleInCity;
+
+console.log(getPeopleInCity);
